36bfb511c7521ce7b129a040dcaaab141cbee2a7
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-17 15:10:08 +0100

    fix: button locales

--- javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
+++ javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
@@ -2,10 +2,11 @@ import Component from '@glimmer/component';
 import I18n from 'I18n';
 
 export default class TopicStatusFilterComponenent extends Component {
-    constructor() {
-        super(...arguments);
-        console.log(themePrefix('close_notify.button.label'));
-        console.log(I18n.t('close_notify.button.label'));
-        console.log(I18n.t(themePrefix('close_notify.button.label')));
+    get label() {
+        return I18n.t(themePrefix('close_notify.button.label'));
+    }
+
+    get title() {
+        return I18n.t(themePrefix('close_notify.button.title'));
     }
 }
