46d42e82b9adce911600f3d1f5ac73edbb8082f5
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-17 17:20:45 +0100

    feat: close/open action for button

--- javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
+++ javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
@@ -1,16 +1,56 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { addObserver, removeObserver } from '@ember/object/observers';
+import { ajax } from 'discourse/lib/ajax';
+import { next } from '@ember/runloop';
 
 export default class CloseNotifyComponent extends Component {
-    get label() {
-        return themePrefix('close_notify.button.label');
-    }
-
-    get title() {
-        return themePrefix('close_notify.button.title');
-    }
+    @tracked icon;
+    @tracked title;
+    @tracked label;
 
     constructor() {
         super(...arguments);
-        console.log(this.args?.topic);
+        this.#updateFromTopicStatus();
+        addObserver(
+            this.getTopic(),
+            'closed',
+            this,
+            this.#updateFromTopicStatus,
+        );
+    }
+
+    willDestroy() {
+        removeObserver(
+            this.getTopic(),
+            'closed',
+            this,
+            this.#updateFromTopicStatus,
+        );
+    }
+
+    getTopic() {
+        return this.args.outletArgs.topic;
+    }
+
+    @action
+    async do() {
+        const topic = this.getTopic();
+        const closed = topic.closed;
+        const response = await ajax(`/t/${topic.id}/status`, {
+            type: 'PUT',
+            data: { status: 'closed', enabled: closed ? 'false' : 'true' },
+        });
+        next(() => DiscourseURL.routeTo(topic.url));
+    }
+
+    #updateFromTopicStatus() {
+        const topic = this.getTopic();
+        const closed = topic.closed;
+        const buttonState = closed ? 'open' : 'close';
+        this.icon = closed ? 'unlock' : 'lock';
+        this.title = themePrefix(`close_notify.button.${buttonState}.title`);
+        this.label = themePrefix(`close_notify.button.${buttonState}.label`);
     }
 }
