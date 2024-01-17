f0b28e038b15d493ca2bc203d33c9f14e25d55aa
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-17 15:12:46 +0100

    fix: typo lable and correct name for class

--- javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
+++ javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import I18n from 'I18n';
 
-export default class TopicStatusFilterComponenent extends Component {
+export default class CloseNotifyComponent extends Component {
     get label() {
         return I18n.t(themePrefix('close_notify.button.label'));
     }
