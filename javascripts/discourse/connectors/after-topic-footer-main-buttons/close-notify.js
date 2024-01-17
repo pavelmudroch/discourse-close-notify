bccf4b79af598897d5fd94c078d1e85b59535ce8
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-17 15:15:28 +0100

    fix: I18n removed from js

--- javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
+++ javascripts/discourse/connectors/after-topic-footer-main-buttons/close-notify.js
@@ -1,12 +1,11 @@
 import Component from '@glimmer/component';
-import I18n from 'I18n';
 
 export default class CloseNotifyComponent extends Component {
     get label() {
-        return I18n.t(themePrefix('close_notify.button.label'));
+        return themePrefix('close_notify.button.label');
     }
 
     get title() {
-        return I18n.t(themePrefix('close_notify.button.title'));
+        return themePrefix('close_notify.button.title');
     }
 }
