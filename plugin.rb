4711b684734e3806ac8cd34bf17f104218360bfa
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-19 14:40:23 +0100

    feat: development logs

--- plugin.rb
+++ plugin.rb
@@ -33,6 +33,7 @@ def get_notification_levels
 end
 
 def notify_user(user, topic, data)
+    puts '[notify_close] :: notify_user :: user_id: ' + user.id.to_s + ' topic_id: ' + topic.id.to_s + ' notification_type: ' + Notification.types[:custom].to_s
     Notification.create!(
         notification_type: Notification.types[:custom],
         user_id: user.id,
@@ -47,23 +48,28 @@ def notify_users(owner, topic, status)
         topic_title: topic.title,
         display_username: owner.username,
         message: status,
-        fancy_title: 'some fancy title'
     }.to_json
+    puts '[notify_close] :: notify_users :: begin :: data: ' + data.to_s
+    puts '[notify_close] :: notify_users :: notification_levels: ' + get_notification_levels.inspect
     topic.topic_users.where(notification_level: get_notification_levels).each do |tu|
+        puts '[notify_close] :: notify_users :: tu.user_id: ' + tu.user_id.to_s + ' == ' + owner.id.to_s
         break if tu.user_id == owner.id
         notify_user(tu.user, topic, data)
     end
 end
 
 def process_post_event(post)
+    puts '[notify_close] :: process_post_event :: begin :: ' + post.post_type.to_s + ' == ' + Post.types[:small_action].to_s
     return if post.post_type != Post.types[:small_action]
 
     status = nil
     case post.action_code
     when 'closed.enabled'
+        puts '[notify_close] :: process_post_event :: action: closed.enabled' + ' condition: ' + SiteSetting.notify_on_close.to_s
         return unless SiteSetting.notify_on_close
         status = 'close'
     when 'closed.disabled'
+        puts '[notify_close] :: process_post_event :: action: closed.disabled' + ' condition: ' + SiteSetting.notify_on_open.to_s
         return unless SiteSetting.notify_on_open
         status = 'open'
     end
@@ -71,6 +77,7 @@ def process_post_event(post)
     owner = post.user
     topic = post.topic
 
+    puts '[notify_close] :: process_post_event :: category-condition: ' + is_topic_from_enabled_category(topic).to_s
     return unless is_topic_from_enabled_category(post.topic)
 
     notify_users(owner, topic, status)
@@ -79,7 +86,9 @@ end
 after_initialize do
     DiscourseEvent.on(:post_created) do |post, opts, user|
         begin
+            puts '[notify_close] :: on "post_created" :: begin'
             process_post_event(post)
+            puts '[notify_close] :: on "post_created" :: end'
         rescue => e
             puts '[notify_close] :: on "post_created" :: error: ' + e.message
         end
@@ -87,7 +96,9 @@ after_initialize do
 
     DiscourseEvent.on(:post_edited) do |post, topic_change|
         begin
+            puts '[notify_close] :: on "post_edited" :: begin'
             process_post_event(post)
+            puts '[notify_close] :: on "post_edited" :: end'
         rescue => e
             puts '[notify_close] :: on "post_edited" :: error: ' + e.message
         end
