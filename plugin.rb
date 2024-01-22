cdc70ec026447d8ac5b44d9d11fac2e1c368323f
Pavel Mudroch <Pavel.Mudroch@nova.cz>
2024-01-22 10:47:23 +0100

    feat(0.2.0): version set to 0.2.0

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-close-notify
 # about: Simple plugin to notify users about closed topic
-# version: 0.1.0
+# version: 0.2.0
 # authors: Pavel Mudroch
 # url: https://github.com/pavelmudroch/discourse-close-notify
 
